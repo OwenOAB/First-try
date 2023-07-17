d30d60fb82321a16fc0cc66041cbe11db98d17ec
“Owen <owenabeatty@gmail.com>
2023-07-17 17:34:06 -0400

    WE

--- first program.py
+++ first program.py
@@ -1,2 +1,57 @@
 print("Hello World!")
 print("I am an enormoud turtle")
+Z = 78
+print(78)
+print(78)
+
+a = 3
+print(a)
+a + 7
+print(10)
+
+b = 4
+c = a*4
+print(b+c)
+print(b*c)
+
+ca = c
+print(c)
+
+d = 0.1+0.2
+print(d)
+d = d*10
+print(d)
+
+f = 4+8/2+1
+print(f)
+
+s = "anaconda"
+print (s)
+print(s[2])
+print(s[6])
+print(s[7])
+print(s[2])
+print(s[-a])
+
+a**2
+print(a)
+print(a**2)
+
+b = 3
+s = 5
+print(b+s)
+s 
+print(b - s)
+print(b+s)
+
+e = 4**(0.5)
+print(e)
+
+g = 4
+print(e/g)
+
+h = 23%5
+print(h)
+print(h%2)
+print(h+1)
+
